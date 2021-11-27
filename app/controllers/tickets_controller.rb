acbc9cd9cd7b17befd87001d60c8140e5a6858be
lion-oldriver <w879443a72cj2i@gmail.com>
2021-11-27 03:03:25 +0000

    add_retirement

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,7 +2,7 @@ class TicketsController < ApplicationController
   def new
     raise ActionController::RoutingError, "ログイン状態で TicketsController#new にアクセス"
   end
-  
+
   def create
     event = Event.find(params[:event_id])
     @ticket = current_user.tickets.build do |t|
@@ -12,5 +12,11 @@ class TicketsController < ApplicationController
     if @ticket.save
       redirect_to event, notice: "このイベントに参加表明しました"
     end
+  end
+
+  def destroy
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
+    ticket.destroy!
+    redirect_to event_path(params[:event_id]), notice: "このイベントの参加をキャンセルしました"
   end
 end
